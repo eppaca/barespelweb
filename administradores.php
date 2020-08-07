599b5c166d0bc5c7b63911a83e5ba29c0730eef6
eppaca <eppaca@espe.edu.ec>
2020-08-07 13:44:01 -0500

    login administradores

---
+++ administradores.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>administradores</h1>
+    <h2>login pagina administradores</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque nobis vel deserunt distinctio recusandae soluta esse. Ex debitis deserunt saepe odio nisi iusto, alias at, quasi temporibus quo officiis pariatur.</p>
+</body>
+</html>
