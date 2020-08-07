b957a0b597b5b906ad80701b7ea51932a34c71ec
eppaca <eppaca@espe.edu.ec>
2020-08-07 13:42:31 -0500

    cambio navbar inquilinos

---
+++ inquilinos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>inquilinos</h1>
+    <h2>navbar</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laudantium repellat id rem recusandae, pariatur accusantium quasi iste officia omnis excepturi tenetur corporis eaque nulla nostrum perspiciatis! Dolor, ab! Harum.</p>
+</body>
+</html>
