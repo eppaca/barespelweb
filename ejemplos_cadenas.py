81497928b23be436db3062dfc6aeaddcd2790a4a
eppaca <eppaca@espe.edu.ec>
2020-08-11 11:41:51 -0500

    commit dia2_python

--- ejemplos_cadenas.py
+++
@@ -1,6 +0,0 @@
-micadena = "hola mundo"
-micadena
-micadena[0]
-micadena[8]
-micadena[9]
-micadena
--- inquilinos.php
+++ inquilinos.php
@@ -8,6 +8,7 @@
 <body>
     <h1>inquilinos</h1>
     <h2>navbar</h2>
+    <p>hacer crud</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laudantium repellat id rem recusandae, pariatur accusantium quasi iste officia omnis excepturi tenetur corporis eaque nulla nostrum perspiciatis! Dolor, ab! Harum.</p>
 </body>
 </html>
